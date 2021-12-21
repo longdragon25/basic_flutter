4cda37ed26726e0291d0935c49efc4e2f8d25e01
longdragon25 <longdragon25112000xt@gmail.com>
2021-12-21 17:43:28 +0700

    demo_redux

---
+++ demo_redux/android/app/src/main/kotlin/com/example/demo_redux/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_redux
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo_redux/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+enum Actions { Increment }
+
+int counterReducer(int state, dynamic action) {
+  if (action == Actions.Increment) {
+    return state + 1;
+  }
+
+  return state;
+}
+
+void main() {
+  final store = new Store<int>(counterReducer, initialState: 0);
+
+  runApp(new FlutterReduxApp(
+    title: 'Flutter Redux Demo',
+    store: store,
+  ));
+}
+
+class FlutterReduxApp extends StatelessWidget {
+  final Store<int> store;
+  final String title;
+
+  FlutterReduxApp({Key? key, required this.store, required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new StoreProvider<int>(
+      store: store,
+      child: new MaterialApp(
+        theme: new ThemeData.dark(),
+        title: title,
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text(title),
+          ),
+          body: new Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                new Text(
+                  'You have pushed the button this many times:',
+                ),
+                new StoreConnector<int, String>(
+                  converter: (store) => store.state.toString(),
+                  builder: (context, count) {
+                    return new Text(
+                      count,
+                      style: TextStyle(fontSize: 15),
+                    );
+                  },
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: new StoreConnector<int, VoidCallback>(
+            converter: (store) {
+              return () => store.dispatch(Actions.Increment);
+            },
+            builder: (context, callback) {
+              return new FloatingActionButton(
+                onPressed: callback,
+                tooltip: 'Increment',
+                child: new Icon(Icons.add),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
